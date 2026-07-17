c6793c5199a1c4d067c94b0ce892f0c85ea75f23
Vinay Kumar <vvinnu261@gmail.com>
2026-07-17 11:38:09 +0530

    Modified

--- 2.py
+++ 2.py
@@ -1,26 +1,26 @@
-# # Constructor and self keyword 
+# Constructor and self keyword 
 
-# # Constructor :- The __init__() method in Python is a special method that initializes an object when it’s created. 
-# # It’s called automatically when you create a new instance of a class.
-# # Basically in a class creating a object using constructor 
+# Constructor :- The __init__() method in Python is a special method that initializes an object when it’s created. 
+# It’s called automatically when you create a new instance of a class.
+# Basically in a class creating a object using constructor 
 
-# # Example 
+# Example 
 
-# class name:
-#     def __init__(self,name1,name2):     # Constructor 
-#         self.name1 = name1
-#         self.name2 = name2
+class name:
+    def __init__(self,name1,name2):     # Constructor 
+        self.name1 = name1
+        self.name2 = name2
 
 
-# class Body:
-#     def __init__ (self,parts):        # This is a constructor part 
-#         self.parts = parts            
-#     def mouth(self):
-#         print(f"{self.parts} is used to eating and talking")
+class Body:
+    def __init__ (self,parts):        # This is a constructor part 
+        self.parts = parts            
+    def mouth(self):
+        print(f"{self.parts} is used to eating and talking")
 
-# mouth1 = Body("Mouth")
+mouth1 = Body("Mouth")
 
-# mouth1.mouth()
+mouth1.mouth()
 
 # Self Keyword :- 
 # self: Refers to the instance of the class itself, allowing you to access attributes and methods within a class
