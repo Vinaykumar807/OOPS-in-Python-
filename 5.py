1967ca4b682ecafe9d766df3c99655901917e04b
Vinay Kumar <vvinnu261@gmail.com>
2026-07-23 15:51:34 +0530

    modified

--- 5.py
+++ 5.py
@@ -5,116 +5,122 @@
 
 
 
-# # Simple Calculator :- 
+#                       1. Simple Calculator :- 
 
-# def add(a,b):
-#     return a+b
-# def sub(a,b):
-#     return a-b
-# def mul(a,b):
-#     return a*b
-# def div(a,b):
-#     return a/b
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
 
-# def display_menu():
-#     print("***Simple Calculator***")  
-#     print("1. Addition")
-#     print("2. Subtraction")
-#     print("3. Multiplication")
-#     print("4. Division")
-#     print("5. Exit!")
+def display_menu():
+    print("***Simple Calculator***")  
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit!")
 
-# while (True) :
-#     display_menu()
+while (True) :
+    display_menu()
 
-#     choice = int(input("Enter your choice (1-5): "))
+    choice = int(input("Enter your choice (1-5): "))
 
-#     if choice in {1,2,3,4}:
-#             a = int(input("Enter your first number : "))
-#             b = int(input("Enter your second number : "))
+    if choice in {1,2,3,4}:
+            a = int(input("Enter your first number : "))
+            b = int(input("Enter your second number : "))
 
-#     if choice == 1:
-#             print("Result :",add(a,b))
-#     elif choice == 2:
-#             print("Result :",sub(a,b))
-#     elif choice == 3:
-#             print("Result :",mul(a,b))
-#     elif choice == 4:
-#             print("Result :",div(a,b))
-#     elif choice == 5 :
-#             print("Exit!!")
-#             break
-#     else :
-#             print("Invaild chioce try agian!!!")
+    if choice == 1:
+            print("Result :",add(a,b))
+    elif choice == 2:
+            print("Result :",sub(a,b))
+    elif choice == 3:
+            print("Result :",mul(a,b))
+    elif choice == 4:
+            print("Result :",div(a,b))
+    elif choice == 5 :
+            print("Exit!!")
+            break
+    else :
+            print("Invaild chioce try agian!!!")
 
 
-# # Banking system 
 
-# def display_menu():
-#     print("***Banking System***")
-#     print("1.Check balance")
-#     print("2.Deposite Money")
-#     print("3.Withdraw Money")
-#     print("4.Exit!!")
-# balance = 0 
+#                            2. Banking system :- 
 
-# while True :
-#     display_menu()
+def display_menu():
+    print("***Banking System***")
+    print("1.Check balance")
+    print("2.Deposite Money")
+    print("3.Withdraw Money")
+    print("4.Exit!!")
+balance = 0 
 
-#     choice = int(input("Enter your Choice(1-4) :"))
+while True :
+    display_menu()
 
-#     if choice == 1:
-#         print(f"Total Balance = ",balance)
-#     elif choice == 2 :
-#         amount = int(input("Enter deposite amount = "))
-#         balance += amount
+    choice = int(input("Enter your Choice(1-4) :"))
+
+    if choice == 1:
+        print(f"Total Balance = ",balance)
+    elif choice == 2 :
+        amount = int(input("Enter deposite amount = "))
+        balance += amount
       
-#     elif choice == 3:
-#         amount = int(input("Enter your Withdraw amount = "))
-#         balance -= amount
+    elif choice == 3:
+        amount = int(input("Enter your Withdraw amount = "))
+        balance -= amount
        
-#     elif choice == 4:
-#         print("Exit!")
-#         break
-#     else :
-#         print("Invalid option Try again!")
+    elif choice == 4:
+        print("Exit!")
+        break
+    else :
+        print("Invalid option Try again!")
 
-# Grocery store menu 
 
-# def store_menu():
-#     print("1. Add items to there carts ")
-#     print("2.Remove items")
-#     print("3.View the total price")
-#     print("4.Exit")
 
-# cart = []
+#                        # 3.  Grocery store menu :- 
 
-# while True :
-#     store_menu()
 
-#     choice = int(input("Enter your chioce (1-4) :"))
+def store_menu():
+    print("1. Add items to there carts ")
+    print("2.Remove items")
+    print("3.View the total price")
+    print("4.Exit")
 
-#     if choice == 1 :
-#         item  = input("Enter your items to add : ")
-#         price = input ("Enter your price : ")
-#         cart.append(item)
+cart = []
+
+while True :
+    store_menu()
+
+    choice = int(input("Enter your chioce (1-4) :"))
+
+    if choice == 1 :
+        item  = input("Enter your items to add : ")
+        price = input ("Enter your price : ")
+        cart.append(item)
         
-#     elif choice == 2:
-#         item = (input("Enter your remove item : "))
-#         if item in cart :
-#             cart.remove(item )
+    elif choice == 2:
+        item = (input("Enter your remove item : "))
+        if item in cart :
+            cart.remove(item )
 
-#     elif choice == 3 :
-#         print("Items : ",cart)
-#         print("price : ",price )
+    elif choice == 3 :
+        print("Items : ",cart)
+        print("price : ",price )
 
-#     elif choice == 4:
-#         print("Exit!")
-#         break
-#     else :
-#         print("Invalid choice try again !")
+    elif choice == 4:
+        print("Exit!")
+        break
+    else :
+        print("Invalid choice try again !")
 
 
+
+#                      4. Library Management System :- 
 
 
 def contact_manage():
