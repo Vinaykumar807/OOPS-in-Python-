df5c7710e3f8e5302a085faac1660033e9e85009
Vinay Kumar <vvinnu261@gmail.com>
2026-08-06 10:49:50 +0530

    Modified

--- 7.py
+++ 7.py
@@ -85,6 +85,7 @@ class Robot(Workable):
 # 5. D = Dependency Inversion Principle(DIP):- 
 # High-level modules should not depend on low-level modules. Both should depend on abstractions.
 # Example :- 
+
 class InputDevice:
     def input(self):
         pass
