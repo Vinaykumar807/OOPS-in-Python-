cf288194f19d0defd02832f229eafc6965ca1f8c
Vinay Kumar <vvinnu261@gmail.com>
2026-08-07 21:55:41 +0530

    Modified

---
+++ 9.py
@@ -0,0 +1,16 @@
+# File handling :- 
+# It allows us to read from and write to files — a common task in almost every real-world application (storing data, logs, reports, etc.).
+
+# Types :- 
+# 'r' :- Read (default mode)	
+# 'w' :-Write (overwrites if file exists)
+# 'a' :-Append (adds content at the end)	
+# 'x' :- Create (fails if file exists)	
+# 'b' :- Binary mode	
+# 't' :- Text mode (default)
+
+# open file 
+file = open("students.txt", "r")
+
+
+
