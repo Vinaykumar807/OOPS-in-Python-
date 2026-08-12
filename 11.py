0042993c582db5bb94be33fe6d684f8bc72a7d35
Vinay Kumar <vvinnu261@gmail.com>
2026-08-12 20:13:51 +0530

    Modified

--- 11.py
+++ 11.py
@@ -1,23 +1,23 @@
-# # Decorators :- 
-# # Decorators allow us to modify the behavior of a function without changing its actual code.
+# Decorators :- 
+# Decorators allow us to modify the behavior of a function without changing its actual code.
 
-# # Example :- 
+# Example :- 
 
-# def welcome(func):
-#     def wrapper():             # This is a special function(we can use any name as a special function )
-#         print("Namaskara!")
-#         func()                 # Here we calling fucn function to wrapp
-#         print("Take care!")
-#     return wrapper
+def welcome(func):
+    def wrapper():             # This is a special function(we can use any name as a special function )
+        print("Namaskara!")
+        func()                 # Here we calling fucn function to wrapp
+        print("Take care!")
+    return wrapper
 
-# @welcome                  # Here we calling a welcome function to decorate and using @ to call the function 
-# def intro():
-#     print("I am Vinay from Karnataka.")
- 
-# intro()                  # Output :-  Namaskara! 
-#                         #             I am Vinay from Karnataka
-#                         #             Take care!
+@welcome                  # Here we calling a welcome function to decorate and using @ to call the function 
+def intro():
+    print("I am Vinay from Karnataka.")
 
+
+intro()                  # Output :-  Namaskara! 
+                        #             I am Vinay from Karnataka
+                        #               Take care!                   
 
 # Decorator with Arguments 
 
